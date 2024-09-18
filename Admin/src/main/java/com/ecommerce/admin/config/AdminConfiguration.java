a88755e8d3b86bddf17a73da2493ee149638d020
DinelyWellehewage <dinelywh78@gmail.com>
2024-09-18 14:44:46 +0200

    configure spring security for admin module

---
+++ Admin/src/main/java/com/ecommerce/admin/config/AdminConfiguration.java
@@ -0,0 +1,91 @@
+package com.ecommerce.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class AdminConfiguration  {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new AdminServiceConfig();
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.authorizeRequests().antMatchers("/*").permitAll()
+//                .antMatchers("/admin/*")
+//                .hasAuthority("ADMIN")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/do-login")
+//                .defaultSuccessUrl("/admin/index")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .invalidateHttpSession(true)
+//                .clearAuthentication(true)
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/login?logout")
+//                .permitAll();
+//    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/*").permitAll()  // Allows access to all paths
+                        .requestMatchers("/admin/*").hasAuthority("ADMIN")  // Restricts access to users with "ADMIN" authority for "/admin/*"
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")  // Custom login page
+                        .loginProcessingUrl("/do-login")  // URL for processing login form submission
+                        .defaultSuccessUrl("/admin/index")  // Default URL on successful login
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))  // Configures the logout URL
+                        .logoutSuccessUrl("/login?logout")  // Redirect URL after successful logout
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+}
--- Library/src/main/java/com/ecommerce/library/model/Role.java
+++ Library/src/main/java/com/ecommerce/library/model/Role.java
@@ -19,4 +19,11 @@ public class Role {
 
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
